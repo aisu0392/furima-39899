73d2826e6031d781a6eaa900ffe04ca79b05eb6f
aisu0392 <aisu00143982@gmail.com>
2024-01-28 12:00:44 +0900

    Fakerを導入

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              { 'test' }
-    email                 { 'testtesttest@example.com' }
-    password              { 'password' }
-    password_confirmation { 'password' }
-    last_name             { '山田' }
-    first_name            { '太郎' }
-    last_name_kana        { 'ヤマダ' }
-    first_name_kana       { 'タロウ' }
-    birthdate             { '1990-01-01' }
+    nickname              { Faker::Name.unique.name }
+    email                 { Faker::Internet.unique.email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name             { Faker::Japanese::Name.last_name }
+    first_name            { Faker::Japanese::Name.first_name }
+    last_name_kana        { Faker::Japanese::Name.last_name_yomi }
+    first_name_kana       { Faker::Japanese::Name.first_name_yomi }
+    birthdate             { Faker::Date.birthday(min_age: 18, max_age: 65).strftime('%Y-%m-%d') }
   end
 end
