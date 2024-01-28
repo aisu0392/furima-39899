b6af02b117faea93f28d21579ef1b53e49ae7343
aisu0392 <aisu00143982@gmail.com>
2024-01-28 18:08:59 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
 
   validates :nickname, presence: true
-  validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[0-9]).+\z/, message: "は半角英数字混合で入力してください" }
+  validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[0-9]).+\z/, message: "は半角英数字混合で入力してください" }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ヴァヶー]+\z/, message: "は全角文字を使用してください" }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナ文字を使用してください" }
   validates :birthdate, presence: true
