cfac9bdc490c1f24cd6e06e98d646e42cbc87d39
aisu0392 <aisu00143982@gmail.com>
2024-01-31 21:19:42 +0900

    画像アップロード機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,9 @@ class ItemsController < ApplicationController
     @items = Item.new
   end
   
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :image, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_duration_id, :price)
+  end
 end
