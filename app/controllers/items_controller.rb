2ce8f7003dd4a758e6f725384327379c65938895
aisu0392 <aisu00143982@gmail.com>
2024-02-03 22:07:45 +0900

    商品出品をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,16 @@ class ItemsController < ApplicationController
   def new
     @items = Item.new
   end
+
+  def create
+    @items = Item.new(item_params)
+
+    if @items.save
+      redirect_to root_path, notice: '商品を出品しました。'
+    else
+      render :new
+    end
+  end
   
   private
 
