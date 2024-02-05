50df524376f5ef9bcbef760287d9933f1df9ec09
aisu0392 <aisu00143982@gmail.com>
2024-02-05 19:57:38 +0900

    小数点切り捨て表示

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -5,6 +5,7 @@ document.addEventListener('turbo:load', () => {
 
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
+    
     if (!inputValue || isNaN(inputValue)) {
       addTaxPrice.innerHTML = "NaN";
       profitPrice.innerHTML = "NaN";
@@ -13,12 +14,12 @@ document.addEventListener('turbo:load', () => {
 
     // 販売手数料と販売利益の計算
     const price = parseFloat(inputValue);
-    const fee = price * 0.1;  // 10%の手数料として計算（適宜変更してください）
-    const profit = price - fee;
+    const fee = Math.floor(price * 0.1);  
+    const profit = Math.floor(price - fee);
 
     // 結果を表示
-    addTaxPrice.innerHTML = `${fee.toFixed(2)}円`;
-    profitPrice.innerHTML = `${profit.toFixed(2)}円`;
+    addTaxPrice.innerHTML = `${fee}円`;
+    profitPrice.innerHTML = `${profit}円`;
 
   });
 });
