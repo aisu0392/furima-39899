7c3a984fef1f4b305f5d556c25c61c6e97fbea58
aisu0392 <aisu00143982@gmail.com>
2024-02-06 20:58:42 +0900

    修正完了

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :item do
+    name { "商品名" }
+    description { "商品の説明" }
+    category { Category.create(name: "カテゴリー名") }
+    condition { Condition.create(name: "状態名") }
+    shipping_fee { ShippingFee.create(name: "送料の負担名") }
+    prefecture { Prefecture.create(name: "都道府県名") }
+    shipping_duration { ShippingDuration.create(name: "発送までの日数名") }
+    price { 5000 }
+    user { FactoryBot.create(:user) }
+
     after(:build) do |item|
       item.image.attach(io: File.open('app/assets/images/test_image.png'), filename: 'test_image.png', content_type: 'image/png')
     end
