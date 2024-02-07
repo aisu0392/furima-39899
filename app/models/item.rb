c244049377036ddb3349b1efda2144620701e69c
aisu0392 <aisu00143982@gmail.com>
2024-02-07 21:13:53 +0900

    追加修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,6 +20,7 @@ class Item < ApplicationRecord
   validates :prefecture, presence: true
   validates :shipping_duration, presence: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :category_id,numericality: { other_than: 1 , message: "can't be blank"} 
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
