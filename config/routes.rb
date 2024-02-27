2a75ee2c038851b02bcf5e7047b5b2b1bc7fba73
aisu0392 <aisu00143982@gmail.com>
2024-02-27 21:09:56 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root 'items#index'
   devise_for :users
   resources :items
+  
+  resources :items do
+    resources :purchases, only: [:new, :create, :index]
+  end
+  resources :purchases, only: [:show]
 end
