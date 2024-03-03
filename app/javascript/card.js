5eb511c6bd4ea9f2b206de1181f07bee7dcac0be
aisu0392 <aisu00143982@gmail.com>
2024-03-03 17:45:35 +0900

    不要コードの削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -32,15 +32,3 @@ const pay = () => {
 window.addEventListener("turbo:load", pay);
 window.addEventListener("turbo:render", pay);      
       
-//       }
-//       numberElement.clear();
-//       expiryElement.clear();
-//       cvcElement.clear();
-//       document.getElementById("charge-form").submit();
-//     });
-//     e.preventDefault();
-//   });
-// };
-
-// window.addEventListener("turbo:load", pay);
-// window.addEventListener("turbo:render", pay);
