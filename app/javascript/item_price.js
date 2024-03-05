419ff261f3be25d004e27dfa1d727e15fa697695
aisu0392 <aisu00143982@gmail.com>
2024-03-05 20:31:52 +0900

    修正完了

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,10 +1,13 @@
-document.addEventListener('turbo:load', () => {
+document.addEventListener('input', (event) => {
   const priceInput = document.getElementById("item-price");
   const addTaxPrice = document.getElementById("add-tax-price");
   const profitPrice = document.getElementById("profit");
 
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
+  if (event.target !== priceInput) {
+    return;
+  }
+
+  const inputValue = priceInput.value;
     
     if (!inputValue || isNaN(inputValue)) {
       addTaxPrice.innerHTML = "NaN";
@@ -18,8 +21,6 @@ document.addEventListener('turbo:load', () => {
     const profit = Math.floor(price - fee);
 
     // 結果を表示
-    addTaxPrice.innerHTML = `${fee}円`;
-    profitPrice.innerHTML = `${profit}円`;
-
-  });
+    addTaxPrice.innerHTML = `${fee}`;
+    profitPrice.innerHTML = `${profit}`;
 });
